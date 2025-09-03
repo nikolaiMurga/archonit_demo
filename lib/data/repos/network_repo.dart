e4690ac424445a1be4f91e21c072db468716d442
nikolaiMurga <nicolai.murga@gmail.com>
2025-09-03 16:32:47 +0200

    [WIP] network repo - implemented network repo - implemented error model - added error handling for network repo - implemented logging service

--- lib/data/repos/network_repo.dart
+++ lib/data/repos/network_repo.dart
@@ -1,13 +1,43 @@
-import 'package:archonit_demo/data/network/api_client.dart';
-import 'package:archonit_demo/data/network/endpoints.dart';
+import 'dart:convert';
+
+import '../../app/logging_service.dart';
+import '../../domain/models/error_model.dart';
+import '../network/api_client.dart';
+import '../network/endpoints.dart';
 
 class NetworkRepo {
   final ApiClient _apiClient;
 
   NetworkRepo(this._apiClient);
 
+  Future<String> _handleResponseStatus({required Future<dynamic> apiCall}) async {
+    final stopwatch = Stopwatch()..start();
+    final response = await apiCall;
+    stopwatch.stop();
+    final elapsedTime = stopwatch.elapsedMilliseconds;
+
+    LogService.addLog('+/SERVER RESPONSE DELAY: $elapsedTime MILLISECONDS /+');
+    LogService.addLog('${response.statusCode} => ${response.reasonPhrase}');
+
+    String body = utf8.decode(response.bodyBytes);
+    switch (response.statusCode) {
+      case (200):
+        break;
+      case (>= 400 && < 500 && != 422 && != 401):
+        throw ErrorModel.fromJson(jsonDecode(body));
+      case (401):
+        throw ErrorModel.fromJson(jsonDecode(body));
+      case (422):
+      // throw ErrorDto(detail: ValidationErrorDto.fromJson(jsonDecode(body)).detail!.first.msg);
+      case (>= 500 && < 600):
+      // throw ErrorDto(detail: response.reasonPhrase);
+    }
+
+    return body;
+  }
+
   Future<void> fetchCurrenciesResponse() async {
-    final response = await _apiClient.get(url: '${Endpoints.baseUrl}${Endpoints.fetchMarket}');
+    final response = await _handleResponseStatus(apiCall: _apiClient.get(url: '${Endpoints.baseUrl}${Endpoints.fetchMarket}'));
     print('object');
   }
 }
