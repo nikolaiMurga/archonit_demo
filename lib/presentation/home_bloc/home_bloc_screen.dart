a3e972d3620f9431af19ff0973fc2f2e310bd307
nikolaiMurga <nicolai.murga@gmail.com>
2025-09-03 21:22:38 +0200

    [WIP] bloc structure - implemented base bloc structure

---
+++ lib/presentation/home_bloc/home_bloc_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeBlocScreen extends StatelessWidget {
+  const HomeBlocScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
