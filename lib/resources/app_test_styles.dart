411b49bc0dd5cc217a94a61f02748ebbd0b850e3
nikolaiMurga <nicolai.murga@gmail.com>
2025-09-04 16:22:08 +0200

    [WIP] home page ui - added resources colors, images, styles

---
+++ lib/resources/app_test_styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+class AppTextStyles {
+  static const primary16w600 = TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: AppColors.primaryColor);
+}
