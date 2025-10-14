0cc4a3e83b4c9944eb45effa60ddf6a390ecf266
nikolaiMurga <nicolai.murga@gmail.com>
2025-10-14 08:50:51 +0200

    - added currency id - override hash & ==

--- lib/domain/models/currency.dart
+++ lib/domain/models/currency.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
 class Currency {
+  final String id;
   final String symbol;
   final double priceUsd;
   final Color color;
 
-  Currency({
-    required this.symbol,
-    required this.priceUsd,
-    required this.color,
-  });
+  Currency({required this.id, required this.symbol, required this.priceUsd, required this.color});
 
   @override
-  int get hashCode => symbol.hashCode;
+  int get hashCode => id.hashCode;
+
+  @override
+  bool operator ==(Object other) => other is Currency && id == other.id && symbol == other.symbol;
 }
