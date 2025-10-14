bd7654901b2ef91647130536d1bc597e46f40430
nikolaiMurga <nicolai.murga@gmail.com>
2025-10-14 12:21:54 +0200

    - implemented removing from favorites

--- lib/presentation/favorites/bloc/favorites_cubit.dart
+++ lib/presentation/favorites/bloc/favorites_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:archonit_demo/domain/models/currency.dart';
 import 'package:archonit_demo/domain/models/error_model.dart';
 import 'package:archonit_demo/domain/use_cases/favorites_use_case.dart';
+import 'package:archonit_demo/presentation/home/bloc/home_cubit.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
@@ -18,4 +19,33 @@ class FavoritesCubit extends Cubit<FavoritesState> {
     final loadedList = _favoritesUseCase.loadFavoriteCurrencies();
     emit(FavoritesState(favoritesList: loadedList));
   }
+
+  Future<void> updateFavoriteCurrencies({required Currency model}) async {
+    // check if model is in favorites
+    final isModelInFavorites = state.favoritesList.contains(model);
+    if (isModelInFavorites) {
+      // if model is in favorites remove model
+      final updatedList = state.favoritesList.where((curr) => curr != model).toList();
+      // cache updated list without model
+      await _favoritesUseCase.saveFavoritesCurrencies(list: updatedList);
+      // update state
+      emit(FavoritesState(favoritesList: updatedList));
+    } else {
+      // if no, save model to favorites
+      state.favoritesList.add(model);
+      try {
+        // cached favorites list
+        await _favoritesUseCase.saveFavoritesCurrencies(list: state.favoritesList);
+      } catch (e) {
+        emit(
+          FavoritesState(
+            favoritesList: state.favoritesList,
+            error: ErrorModel(message: e.toString()),
+          ),
+        );
+      }
+    }
+  }
+
+  bool isFavorite(Currency curr) => state.favoritesList.contains(curr);
 }
--- lib/presentation/home/widgets/currency_card.dart
+++ lib/presentation/home/widgets/currency_card.dart
@@ -1,16 +1,23 @@
 import 'package:archonit_demo/domain/models/currency.dart';
 import 'package:archonit_demo/presentation/favorites/bloc/favorites_cubit.dart';
-import 'package:archonit_demo/presentation/home/bloc/home_cubit.dart';
 import 'package:archonit_demo/resources/app_test_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CurrencyCard extends StatelessWidget {
+class CurrencyCard extends StatefulWidget {
   final Currency currency;
 
   const CurrencyCard({super.key, required this.currency});
 
+  @override
+  State<CurrencyCard> createState() => _CurrencyCardState();
+}
+
+
+
+class _CurrencyCardState extends State<CurrencyCard> {
   void _showContextMenu(BuildContext context, Offset position, Currency model) async {
+    final _isFavorite = context.read<FavoritesCubit>().state.favoritesList.contains(widget.currency);
     final RenderBox overlay = Overlay.of(context).context.findRenderObject() as RenderBox;
     final result = await showMenu(
       context: context,
@@ -18,7 +25,13 @@ class CurrencyCard extends StatelessWidget {
       items: [
         PopupMenuItem(
           value: 'favorite',
-          child: Row(children: [Icon(Icons.favorite_border), SizedBox(width: 8), Text('add to favorites')]),
+          child: Row(
+            children: [
+              Icon(_isFavorite ? Icons.favorite_border : Icons.favorite),
+              SizedBox(width: 8),
+              Text(_isFavorite ? 'remove from favorites' : 'add to favorites'),
+            ],
+          ),
         ),
       ],
       elevation: 8,
@@ -26,16 +39,18 @@ class CurrencyCard extends StatelessWidget {
     );
 
     if (result == 'favorite') {
-      await context.read<HomeCubit>().saveFavoriteCurrencies(model: currency);
+      await context.read<FavoritesCubit>().updateFavoriteCurrencies(model: widget.currency);
+      // temp solution to rebuild card on home screen
+      setState(() {});
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    final _isFavorite = context.read<FavoritesCubit>().state.favoritesList.contains(currency);
+    final _isFavorite = context.watch<FavoritesCubit>().isFavorite(widget.currency);
     return GestureDetector(
       behavior: HitTestBehavior.opaque,
-      onLongPressStart: (details) => _showContextMenu(context, details.globalPosition, currency),
+      onLongPressStart: (details) => _showContextMenu(context, details.globalPosition, widget.currency),
       child: SizedBox(
         height: 84,
         child: Padding(
@@ -45,15 +60,15 @@ class CurrencyCard extends StatelessWidget {
               Container(
                 height: 56,
                 width: 56,
-                decoration: BoxDecoration(color: currency.color, borderRadius: BorderRadius.circular(18)),
+                decoration: BoxDecoration(color: widget.currency.color, borderRadius: BorderRadius.circular(18)),
               ),
               Padding(
                 padding: const EdgeInsets.only(left: 16.0),
-                child: Text(currency.symbol, style: AppTextStyles.text16w600),
+                child: Text(widget.currency.symbol, style: AppTextStyles.text16w600),
               ),
               if (_isFavorite) Padding(padding: const EdgeInsets.all(8.0), child: Icon(Icons.favorite, size: 12)),
               const Expanded(child: SizedBox()),
-              Text('\$${currency.priceUsd.toStringAsFixed(2)}', style: AppTextStyles.text16w600),
+              Text('\$${widget.currency.priceUsd.toStringAsFixed(2)}', style: AppTextStyles.text16w600),
             ],
           ),
         ),
